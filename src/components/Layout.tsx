c1336276d74c1fc8240dce636e0ec7472a10d95b
rohitsaxena <rohitmsaxena@gmail.com>
2025-02-08 03:01:15 -0700

    - added icon - implemented scroll

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -15,6 +15,28 @@ export default function Layout({ children }: LayoutProps) {
           <div className="flex-none">
             <ul className="menu menu-horizontal px-1">
               <li>
+                <a
+                  href="https://www.linkedin.com/in/rohitsaxena42/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center space-x-2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.75 3a1.75 1.75 0 1 1 0 3.5A1.75 1.75 0 0 1 4.75 3zM3 8.25h3.5v12.5H3V8.25zM8.75 8.25H12v1.526c.534-.978 1.942-2.026 3.934-2.026 4.2 0 4.966 2.71 4.966 6.235v6.765h-3.5v-6.125c0-1.462-.026-3.34-2.05-3.34-2.053 0-2.37 1.605-2.37 3.22v6.245h-3.5V8.25z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  {/*<span>My LinkedIn</span>*/}
+                </a>
+              </li>
+              <li>
                 <a href="#work-experience">Work Experience</a>
               </li>
               <li>
