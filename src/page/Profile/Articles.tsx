54c914e4690a81ac8a5fa898cff80f7b82361d27
rohitsaxena <rohitmsaxena@gmail.com>
2025-02-11 04:06:21 -0700

    - added under develompent sections

--- src/page/Profile/Articles.tsx
+++ src/page/Profile/Articles.tsx
@@ -3,7 +3,7 @@ import ProfileSection from "../../components/ProfileSection";
 export default function Articles() {
   return (
     <ProfileSection title={"Articles"} sectionId={"articles"}>
-      articles
+      🚧 Under Development 🚧
     </ProfileSection>
   );
 }
--- src/page/Profile/CAD.tsx
+++ src/page/Profile/CAD.tsx
@@ -3,7 +3,7 @@ import ProfileSection from "../../components/ProfileSection";
 export default function CAD() {
   return (
     <ProfileSection title={"CAD"} sectionId={"cad"}>
-      cad
+      🚧 Under Development 🚧
     </ProfileSection>
   );
 }
